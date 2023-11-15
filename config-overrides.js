5f2fe4d4898be75c127832f06f6be93d9615a837
D-Sketon <2055272094@qq.com>
2023-11-15 15:41:02 +0800

    chore: update deps

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+// config-overrides.js
+const {
+  override,
+  addLessLoader,
+  adjustStyleLoaders,
+} = require("customize-cra");
+
+module.exports = {
+  webpack: override(
+    addLessLoader({
+      lessOptions: {
+        javascriptEnabled: true,
+      },
+    }),
+    adjustStyleLoaders(({ use: [, , postcss] }) => {
+      const postcssOptions = postcss.options;
+      postcss.options = { postcssOptions };
+    }),
+  ),
+};
