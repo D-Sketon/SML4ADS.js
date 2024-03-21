9089d2e48e043468bbd0aa11814936f3c3320f85
D-Sketon <2055272094@qq.com>
2024-03-21 21:33:37 +0800

    fix: tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './public/index.html',
     "./src/**/*.{js, jsx, ts, tsx}",
+    "./src/**/*.tsx",
+    "./src/**/*.ts",
   ],
   theme: {
     extend: {},
